c7d44a5dbf6ca2ae68f56da986c9bc20e9a9b68e
Konstantin-174 <timokhin_74@hotmail.com>
2021-05-11 21:53:41 +0500

    5th (Added useState for InputBlock)

--- src/components/InputBlock/InputBlock.tsx
+++ src/components/InputBlock/InputBlock.tsx
@@ -1,11 +1,30 @@
 import local from './InputBlock.module.scss';
-import React from 'react';
+import React, {useState} from 'react';
 import CounterButton from '../Counter/CounterButton/CounterButton';
 
 export const InputBlock: React.FC = () => {
 
+    let [maxValue, setMaxValue] = useState<number>(0)
+    let [startValue, setStartValue] = useState<number>(0)
+
+    const decMaxValue = () => {
+        setMaxValue(maxValue - 1)
+    }
+
+    const incMaxValue = () => {
+        setMaxValue(maxValue + 1)
+    }
+
+    const decStartValue = () => {
+        setStartValue(startValue - 1)
+    }
+
+    const incStartValue = () => {
+        setStartValue(startValue + 1)
+    }
+
     const setBtn = () => {
-        alert("WTF!? Where is my value?")
+        alert('WTF!? Where is my value?')
     }
 
     return (
@@ -15,13 +34,19 @@ export const InputBlock: React.FC = () => {
                     <div className={local.inputTitle}>
                         max value
                     </div>
-                    <Input />
+                    <Input value={maxValue}
+                           decValue={decMaxValue}
+                           incValue={incMaxValue}
+                    />
                 </div>
                 <div className={local.inputItem}>
                     <div className={local.inputTitle}>
                         start value
                     </div>
-                    <Input />
+                    <Input value={startValue}
+                           decValue={decStartValue}
+                           incValue={incStartValue}
+                    />
                 </div>
             </div>
             <div className={local.btn}>
@@ -31,17 +56,31 @@ export const InputBlock: React.FC = () => {
     )
 }
 
-const Input: React.FC = () => {
+type InputPropsType = {
+    value: number
+    incValue: () => void
+    decValue: () => void
+}
+
+const Input: React.FC<InputPropsType> = ({value, incValue, decValue}) => {
+
 
     return (
         <div className={local.input}>
             <div className={local.dec}
                  tabIndex={0}
-                 >-</div>
-            <input type="text"
+                 onClick={decValue}
+            >
+                -
+            </div>
+            <input type="text" value={value}
             />
             <div className={local.inc}
-                 tabIndex={0}>+</div>
+                 tabIndex={0}
+                 onClick={incValue}
+            >
+                +
+            </div>
         </div>
     )
 }
